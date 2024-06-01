ccb2619a8d80ed3847fe7caf1f3be1845af08a49
Per Cederberg <cederberg@gmail.com>
2024-06-01 19:14:02 +0200

    Updated process RSS + time calculations to use sysinfo more

--- src/proc.rs
+++ src/proc.rs
@@ -3,11 +3,17 @@ use regex::RegexBuilder;
 use std::collections::HashMap;
 use sysinfo::System;
 
+pub struct ProcessInfo {
+    cmd: String,
+    cpu: u64,
+    rss: u64,
+}
+
 pub struct ProcessMap {
     roots: Vec<u32>,
     parents: HashMap<u32, u32>,
     children: HashMap<u32, Vec<u32>>,
-    cmds: HashMap<u32, String>,
+    info: HashMap<u32, ProcessInfo>,
 }
 
 impl ProcessMap {
@@ -15,18 +21,28 @@ impl ProcessMap {
         let mut roots = vec![];
         let mut parents = HashMap::new();
         let mut children = HashMap::new();
-        let mut cmds = HashMap::new();
+        let mut info = HashMap::new();
         for (pid, proc) in sys.processes() {
-            children.entry(pid.as_u32()).or_insert(vec![]);
-            cmds.insert(pid.as_u32(), proc.cmd().join(" "));
-            if let Some(ppid) = proc.parent() {
-                parents.insert(pid.as_u32(), ppid.as_u32());
-                children.entry(ppid.as_u32()).or_insert(vec![]).push(pid.as_u32());
-            } else {
-                roots.push(pid.as_u32());
+            if proc.thread_kind().is_none() {
+                children.entry(pid.as_u32()).or_insert(vec![]);
+                let cmd = proc.cmd().join(" ");
+                let mut cpu = 0;
+                let rss = proc.memory();
+                // FIXME: Ideally sysinfo should provide this instead...
+                let ps = procfs::process::Process::new(pid.as_u32() as i32);
+                if let Ok(stat) = ps.and_then(|p| p.stat()) {
+                    cpu = (stat.utime + stat.stime) / procfs::ticks_per_second();
+                }
+                info.insert(pid.as_u32(), ProcessInfo { cmd, cpu, rss });
+                if let Some(ppid) = proc.parent() {
+                    parents.insert(pid.as_u32(), ppid.as_u32());
+                    children.entry(ppid.as_u32()).or_insert(vec![]).push(pid.as_u32());
+                } else {
+                    roots.push(pid.as_u32());
+                }
             }
         }
-        return ProcessMap { roots, parents, children, cmds };
+        return ProcessMap { roots, parents, children, info };
     }
 
     fn is_service(&self, pid: &u32) -> bool {
@@ -55,34 +71,28 @@ impl ProcessMap {
     }
 
     pub fn service_by_pid(&self, pid: &u32) -> Option<u32> {
-        return self.cmds.contains_key(pid).then(|| self.as_service(pid));
+        return self.info.contains_key(pid).then(|| self.as_service(pid));
     }
 
     pub fn service_by_cmd(&self, cmd: &str) -> Option<u32> {
         let re = RegexBuilder::new(cmd).case_insensitive(true).build().unwrap();
         let is_match = |s: &String| s.contains(cmd) || re.is_match(s);
-        let found = self.cmds.iter().filter_map(|(k, v)| is_match(v).then(|| k));
+        let found = self.info.iter().filter_map(|(k, v)| is_match(&v.cmd).then(|| k));
         return found.map(|p| self.as_service(p)).min();
     }
 
     pub fn stat(&self, pid: &u32) -> (u64, u64) {
-        let (cputicks, rssbytes) = self.stat_collect(pid);
-        return (cputicks / procfs::ticks_per_second(), rssbytes);
-    }
-
-    fn stat_collect(&self, pid: &u32) -> (u64, u64) {
-        let mut cputicks = 0;
-        let mut rssbytes = 0;
-        let proc = procfs::process::Process::new(*pid as i32);
-        if let Ok(stat) = proc.and_then(|p| p.stat()) {
-            cputicks += stat.utime + stat.stime;
-            rssbytes += stat.rss_bytes();
+        let mut cpu = 0;
+        let mut rss = 0;
+        if let Some(info) = self.info.get(pid) {
+            cpu += info.cpu;
+            rss += info.rss;
             for cid in self.children.get(pid).unwrap() {
-                let (ticks, bytes) = self.stat_collect(cid);
-                cputicks += ticks;
-                rssbytes += bytes;
+                let (c, r) = self.stat(cid);
+                cpu += c;
+                rss += r;
             }
         }
-        return (cputicks, rssbytes);
+        return (cpu, rss);
     }
 }
